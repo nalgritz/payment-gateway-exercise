5ddcea33cc84a963a0e3cd00f8fb9b2d54c8ca09
nalgritz <nalgritz9270@gmail.com>
2018-04-25 02:18:40 +0800

    Add card-validator, reorganize file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PaymentForm from './PaymentForm.js';
+import PaymentForm from './form/PaymentForm.js';
 
 class App extends Component {
   state = {
