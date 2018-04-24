77e1ada1266638ac344f892d958dc5e02c77f222
nalgritz <nalgritz9270@gmail.com>
2018-04-25 02:44:32 +0800

    Change initial route

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   };
 
   callApi = async () => {
-    const response = await fetch('/api/hello'),
+    const response = await fetch('/payment'),
           body     = await response.json();
 
     if (response.status !== 200) throw Error(body.message);
