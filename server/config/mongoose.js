b348d4de30fc516ec1748e365835bfed6030fe12
Rex Shum <nalgritz9270@gmail.com>
2021-03-07 03:08:06 +0800

    Clear

--- server/config/mongoose.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URL);
-mongoose.connection.on('error', () => {
-    console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
-    process.exit(1);
-});
